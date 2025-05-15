c9d8d838abc1847815bd88148680df388cc57633
d.kulahyn <dmytro.kulahyn@devspire.eu>
2025-05-15 12:46:40 +0200

    main

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/groups/{group}', function () {
-//    return view('redirect-to-store');
-//});
+Route::get('/groups/{group}', function () {
+    return view('redirect-to-store');
+});
