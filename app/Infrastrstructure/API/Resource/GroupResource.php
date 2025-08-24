35549562491edfa227080c1ae6255c65e3db312f
d.kulahyn <dmytro.kulahyn@devspire.eu>
2025-08-24 17:46:16 +0200

    main

--- app/Infrastrstructure/API/Resource/GroupResource.php
+++ app/Infrastrstructure/API/Resource/GroupResource.php
@@ -22,6 +22,10 @@ class GroupResource extends JsonResource
         $resource = $this->resource;
 
         $balances = $resource->getBalances();
+        
+        header('Content-Type: application/json');
+        echo json_encode($balances);die;
+        
         $overallBalances = $resource->getMembers->map(fn (Customer $customer) => $customer->getBalance())->toArray();
 
         $customersWithGroupBalances = $resource->getMembers->map(fn (Customer $customer) => $customer->setBalance($balances[$customer->id]))->toArray();
